0d02f749f3b871a167bcfbcbd996427f410bf7a2
tramwaiter <tramwaiter@yandex.ru>
2022-02-17 09:10:37 +0300

    Инициализирован Spring Boot проект. Closes #1

---
+++ src/main/java/com/vitalmsystems/siguracstesttask/SigurAcsTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.vitalmsystems.siguracstesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigurAcsTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigurAcsTestTaskApplication.class, args);
+	}
+
+}
